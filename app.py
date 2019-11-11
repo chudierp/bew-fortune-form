cb1f1d69c9d98ad5b2d33829ec3190da637651b1
Chudier <chudierp@gmail.com>
2019-11-10 23:45:14 -0800

    homework three completed

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template, request
+import requests
+import pprint
 
 app = Flask(__name__)
+
+pp = pprint.PrettyPrinter(indent=4)
+weather_url = "https://api.openweathermap.org/data/2.5/weather"
 
 @app.route('/')
 def index():
@@ -39,4 +44,36 @@ def fortune_results():
     else:
         fortune = "good luck in life!"
 
-    return render_template('fortune_results.html', fortune=fortune)  
+    return render_template('fortune_results.html', fortune=fortune)
+
+@app.route('/weather')
+def weather_form():
+    """ display the weather form page"""
+    return render_template('weather_form.html') 
+
+@app.route('/weather_results')
+def weather_results_page():
+    users_city = request.args.get('city')
+   
+    params = {
+        'q': users_city,
+        'appid': '2608f679d4594364525f6c6cc2246c79'
+    }
+
+    response = requests.get(weather_url, params=params)
+
+    if not response.status_code == 200:
+        print("error")
+    results = response.json()
+    
+    city = results['name']
+    temp = kelvin_to_farenheit(results['main']['temp'])
+    
+    return render_template('weather_results.html', city=city, temp=temp)
+
+def kelvin_to_farenheit(k):
+    results = 1.8 * (k-273) + 32
+    return int(results)
+
+if __name__ == '__main__':
+    app.run()
